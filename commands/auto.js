d597bf63b5f5cde8afb01761836be2f798cd8d7e
kushan sewmina <115915441+kushansewmina1234@users.noreply.github.com>
2024-04-03 19:47:09 +0530

    Create auto.js

---
+++ commands/auto.js
@@ -0,0 +1,47 @@
+cmd({
+
+            pattern: "hi",           
+            desc: "(menu cmdlist).",
+            category: "_help",
+            react: "👨‍💻",
+            filename: __filename,
+            use: '<faded-Alan walker.>',
+
+        },
+
+        async(Void, citel, text) => {
+        let buttons = [{
+
+                    buttonId: `${prefix}system`,
+                    buttonText: {
+                    displayText: "System",
+                    },
+
+                    type: 1,
+                },
+                  {
+                    buttonId: `ping`,
+                    buttonText: {
+                    displayText: "Ping",
+
+                    },
+                    type: 1,
+                },
+            ];
+            let buttonMessage = {
+                image: {
+                    url: 'https://telegra.ph/file/b10172f9cfdbe5f43c43e.jpg',
+                },
+
+                caption: `
+Hi
+`,
+
+                footer: tlang().footer,
+                headerType: 4,
+            };
+            return Void.sendMessage(citel.chat, buttonMessage, {
+                quoted: citel,
+            });
+        }
+    )
