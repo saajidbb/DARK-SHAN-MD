81a8bd92273ad2bed94ffdf2fda79d5ebb867c5c
kushan sewmina <115915441+kushansewmina1234@users.noreply.github.com>
2024-04-05 20:04:01 +0530

    Update movie.js

--- commands/movie.js
+++ commands/movie.js
@@ -9,10 +9,13 @@ cmd(
         filename: __filename
     },
     async (Void, citel, text) => {
-const input = `${text}` //movie or tvshow name
-const movie await SinhalaSub.get_list.by_search(input)
+        if (!text) return
+try {
+let movie SinhalaSub.get_list.by_search(text)
       
       const cap `movie search results ${movie.results.title} echcharai`
       
       await Void.sendMessage(citel.chat, { caption: cap }, { quoted: citel })
-      })
+        }
+    }
+ )
