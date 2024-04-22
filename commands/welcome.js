c07e5a9e810280d7bcb4f8a98de5b5a014296691
kushan sewmina <115915441+kushansewmina1234@users.noreply.github.com>
2024-04-22 10:32:06 +0530

    Update welcome.js

--- commands/welcome.js
+++ commands/welcome.js
@@ -14,10 +14,10 @@ cmd({
       
       let textw = `◉┉❮❮ 𝙳𝙰𝚁𝙺 𝚂𝙷𝙰𝙽 𝙼𝙳 𝚃𝙴𝙲𝙷 𝙽𝙴𝚆𝚂 ❯❯┉◉\n\n`;
 for (let i=1; i<16; i++){
-  textw +=`❒ 𝐍𝐎 ${news.result.[i].no}\n`
-  textw +=`❒ 𝐂𝐀𝐓𝐍𝐀𝐌𝐄: ${news.result.[i].catname}\n`
-  textw +=`❒ 𝐓𝐈𝐓𝐋𝐄: ${news.result.[i].title}\n`
-  textw +=`❒ 𝐃𝐀𝐓𝐄: ${news.result.[i].date}\n`
+  textw +=`❒ 𝐍𝐎 ${news.result[i].no}\n`
+  textw +=`❒ 𝐂𝐀𝐓𝐍𝐀𝐌𝐄: ${news.result[i].catname}\n`
+  textw +=`❒ 𝐓𝐈𝐓𝐋𝐄: ${news.result[i].title}\n`
+  textw +=`❒ 𝐃𝐀𝐓𝐄: ${news.result[i].date}\n`
 } 
         return await citel.reply(textw );
             
